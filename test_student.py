8f3ad9cc38ef13283296e414b027d75821d93d30
ciaraosull <ciaraosull@gmail.com>
2022-04-16 19:48:31 +0000

    Use setUp & tearDown

--- test_student.py
+++ test_student.py
@@ -4,23 +4,33 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
     "TestStudent class"
+
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        print("tearDown")
+
     def test_full_name(self):
         "Test to check if method prints full name"
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+        # student = Student("John", "Doe") not needed if setUp used
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_email(self):
-        student = Student("John", "Doe")
-
-        self.assertAlmostEqual(student.email, 'john.doe@email.com')
-
+        "Test to check student email method"
+        # student = Student("John", "Doe") not needed if setUp used
+        print("test_email")
+        self.assertAlmostEqual(self.student.email, 'john.doe@email.com')
 
     def test_alert_santa(self):
         "Test to check alert santa method"
-        student = Student("John",  "Doe")
-        student.alert_santa()
+        # student = Student("John",  "Doe") not needed if setUp used
+        print("test_alert_santa")
+        self.student.alert_santa()
 
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
 
 
 if __name__ == "__main__":
